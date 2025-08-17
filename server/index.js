5661a8ea3a7bd5fc07ff4ffcc89c209a34cc0231
RaviD98 <notravi98@gmail.com>
2025-08-17 18:07:56 +0530

    Removed unnecessary comments

--- server/index.js
+++ server/index.js
@@ -1,42 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import connectDB from "./database/db.js";
-// import userRoute from "./routes/user.route.js";
-// import courseRoute from "./routes/course.route.js";
-// import mediaRoute from "./routes/media.route.js"
-// import purchaseRoute from "./routes/purchaseCourse.route.js"
-// import courseProgressRoute from "./routes/courseProgress.route.js";
-
-// dotenv.config({});
-// connectDB();
-
-// const app = express();
-
-// const PORT = process.env.PORT || 3000;
-
-// //Default middleware
-// app.use(express.json());
-// app.use(cookieParser()); //npm i cookie-parser
-// app.use(cors({
-//    origin: "http://localhost:5173",
-//    credentials: true,
-// }));
-
-
-// //APIs
-// app.use("/api/v1/media", mediaRoute);
-// app.use("/api/v1/users", userRoute);
-// app.use("/api/v1/course", courseRoute);
-// app.use("/api/v1/purchase", purchaseRoute);
-// app.use("/api/v1/progress", courseProgressRoute);
-
-
-// app.listen(PORT, () =>{
-//    console.log(`Listening on ${PORT}`); 
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -69,9 +30,11 @@ app.post(
 // Default middleware (after webhook)
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL?.split(",") || ["http://localhost:5173"],
     credentials: true,
   })
 );
